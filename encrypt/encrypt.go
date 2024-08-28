c731d95555d7c16384e69344b6d74ab66b985b35
Edgar Montenegro <edgarmontenegro321@gmail.com>
2024-08-28 05:26:44 -0300

    🎡 Refactoring code

--- encrypt/encrypt.go
+++ encrypt/encrypt.go
@@ -1,7 +1,6 @@
 package encrypt
 
 import (
-	"fmt"
 	"strings"
 )
 
@@ -43,13 +42,11 @@ func RemoveZeroSumSubArrays(arr []int) []int {
 	sums := map[int]int{0: -1}
 	accumulatedSum := 0
 
-	for i, num := range arr {
+	for _, num := range arr {
 		accumulatedSum += num
-		fmt.Printf("Index: %d, Num: %d, AccumulatedSum: %d\n", i, num, accumulatedSum)
 		// If accumulated sum exist in sums (map), the subarray = 0
 		if startIndex, found := sums[accumulatedSum]; found {
 			// Delete subarray
-			fmt.Printf("Removing elements from index %d to %d\n", startIndex+1, len(stack)-1)
 			stack = stack[:startIndex+1]
 		} else {
 			// Add index to current map and stack
@@ -57,6 +54,5 @@ func RemoveZeroSumSubArrays(arr []int) []int {
 			stack = append(stack, num)
 		}
 	}
-
 	return stack
 }
