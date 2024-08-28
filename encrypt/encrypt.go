deacaa3ca2252506b0bc7af6449afe4b26b5a7cf
Edgar Montenegro <edgarmontenegro321@gmail.com>
2024-08-28 02:46:59 -0300

    😎 Add coding solution to question 3

--- encrypt/encrypt.go
+++ encrypt/encrypt.go
@@ -1,6 +1,9 @@
 package encrypt
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 func EncryptMessage(key, message string) string {
 	// Key nil || ""
@@ -28,4 +31,32 @@ func EncryptMessage(key, message string) string {
 		result.WriteRune(char)
 	}
 	return result.String()
+}
+
+func RemoveZeroSumSubArrays(arr []int) []int {
+	if arr == nil {
+		return []int{}
+	}
+
+	stack := []int{}
+	// Map to check accumulated sum
+	sums := map[int]int{0: -1}
+	accumulatedSum := 0
+
+	for i, num := range arr {
+		accumulatedSum += num
+		fmt.Printf("Index: %d, Num: %d, AccumulatedSum: %d\n", i, num, accumulatedSum)
+		// If accumulated sum exist in sums (map), the subarray = 0
+		if startIndex, found := sums[accumulatedSum]; found {
+			// Delete subarray
+			fmt.Printf("Removing elements from index %d to %d\n", startIndex+1, len(stack)-1)
+			stack = stack[:startIndex+1]
+		} else {
+			// Add index to current map and stack
+			sums[accumulatedSum] = len(stack)
+			stack = append(stack, num)
+		}
+	}
+
+	return stack
 }
--- main.go
+++ main.go
@@ -17,4 +17,10 @@ func main() {
 	encryptedMessage := encrypt.EncryptMessage(key, message)
 
 	fmt.Println(encryptedMessage)*/
+
+	arr := []int{3, 4, -7, 5, -6, 2, 5, -1, 8}
+	// arr := []int{1, 2, -3, 3, 1, -1}
+	// arr := []int{2, 3, -5, -2, 7}
+	result := encrypt.RemoveZeroSumSubArrays(arr)
+	fmt.Println(result)
 }
